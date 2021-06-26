bd2de05f9e1b976cf2e44491215ebcff2d76b2c1
chrishdk <daniel.stari@hotmail.com>
2021-06-26 12:03:00 -0400

    a

--- Tienda/core/urls.py
+++ Tienda/core/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 from django.urls.resolvers import URLPattern
-from .views import home,hardware
+from .views import home,hardware, notebook, contacto, acercade, iniciosesion, registro, carrito
 
 
 urlpatterns = [
     path('', home, name="home"),
-    path('home', home, name="home"),
     path('hardware', hardware, name="hardware"),
+    path('Notebook', notebook, name="notebook"),
+    path('Contacto', contacto, name="contacto"),
+    path('AcercaDe', acercade, name="acercade"),
+    path('InicioSesion', iniciosesion, name="iniciosesion"),
+    path('Registro', registro, name="registro"),
+    path('Carrito', carrito, name="carrito"),
 ]
--- Tienda/core/views.py
+++ Tienda/core/views.py
@@ -3,7 +3,26 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request,'core/home.html')
+    return render(request, "core/home.html")
 
 def hardware(request):
-    return render(request,'core/Hardware.html')
+    return render(request, "core/Hardware.html")
+
+    
+def notebook(request):
+    return render(request, "core/Notebooks.html")
+
+def contacto(request):
+    return render(request, "core/Contacto.html")
+
+def acercade(request):
+    return render(request, "core/AcercaDe.html")
+
+def iniciosesion(request):
+    return render(request, "core/InicioSesion.html")
+
+def registro(request):
+    return render(request, "core/Registro.html")
+
+def carrito(request):
+    return render(request, "core/Carrito.html")
