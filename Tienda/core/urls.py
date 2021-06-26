e3fcb658b06a2c756856e94d15a8e894e2c67bde
chrishdk <daniel.stari@hotmail.com>
2021-06-26 11:42:09 -0400

    a

--- Tienda/core/urls.py
+++ Tienda/core/urls.py
@@ -5,6 +5,6 @@ from .views import home,hardware
 
 urlpatterns = [
     path('', home, name="home"),
-    path('home.html', home, name="home"),
-    path('Hardware.html', hardware, name="hardware"),
+    path('home', home, name="home"),
+    path('hardware', hardware, name="hardware"),
 ]
