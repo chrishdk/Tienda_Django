aa9751f89b15750c7fcbaf27a9e362544a0808d9
chrishdk <daniel.stari@hotmail.com>
2021-06-26 20:26:22 -0400

    models

--- Tienda/core/models.py
+++ Tienda/core/models.py
@@ -5,7 +5,7 @@ from django.db.models.query import FlatValuesListIterable
 
 
 #  crear modelo para categoria
-class categoria(models.Model):
+class Categoria(models.Model):
     idCategoria =models.IntegerField(primary_key=True, verbose_name="Id de categoria")
     nombreCategoria = models.CharField(max_length=80, blank=False, null=False, verbose_name="Nombre categoria")
 
@@ -19,7 +19,7 @@ class Productos(models.Model):
     nomProducto = models.CharField(max_length=15, blank=False, null=False, verbose_name="Nombre producto")
     imagen = models.ImageField(upload_to="images/",default="sinfoto.png", null=False, blank=False, verbose_name="Imagen producto")
     precioProducto = models.IntegerField(max_length=8, null=False,blank=False, verbose_name="Precio Producto" )
-    categoria = models.ForeignKey(categoria, on_delete=models.DO_NOTHING)
+    categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)
 
     def __str__(self):
         return self.codProducto
