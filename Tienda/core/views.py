9e20cfb94a62120f3d6cee6a82f2b48d49a0c233
Notwell1 <82486010+Notwell1@users.noreply.github.com>
2021-07-02 19:08:20 -0400

    DescFich

--- Tienda/core/views.py
+++ Tienda/core/views.py
@@ -115,14 +115,53 @@ def poblar_bd(request):
                             imagen="images/Notebook/001/001.png",
                             precioProducto="1099990",
                             precioProductoCred="1249990",
-                            descFich="lala"  ,
+                            descFich="AMD Ryzen 7 4800H / RAM 16 GB DDR4 / Pantalla LED 15.6'' / NVIDIA GeForce RTX 2060 /"  ,
                             descFichCom="lala",
                             categoria=Categoria.objects.get(idCategoria=2))
 
-
-    Producto.objects.create(codProducto="2", nomProducto='ASUS TUF Gaming F15',        imagen="images/Notebook/002/002.png",      precioProducto="869990"  , precioProductoCred="899990",                          descFich="lala"  ,descFichCom="lala",                  categoria=Categoria.objects.get(idCategoria=2))
-    Producto.objects.create(codProducto="3", nomProducto='ASUS Chromebook C423NA-WB04',         imagen="images/Notebook/003/003.png",      precioProducto="209990", precioProductoCred="209990",                   descFich="lala"  ,descFichCom="lala",                 categoria=Categoria.objects.get(idCategoria=2))
-    Producto.objects.create(codProducto="4", nomProducto='Intel Core i7-11700F',         imagen="images/Procesador/001/001.png",      precioProducto="319900", precioProductoCred="319900",                        descFich="lala"  ,descFichCom="lala",                  categoria=Categoria.objects.get(idCategoria=1))
-    Producto.objects.create(codProducto="5", nomProducto='GSkill Trident Z',        imagen="images/ram/001/001.png", imagen2="images/ram/001/001c.png",      precioProducto="59990"  , precioProductoCred="59990", descFich="lala"  ,descFichCom="lala",                  categoria=Categoria.objects.get(idCategoria=1))
-    Producto.objects.create(codProducto="6", nomProducto='MSI GeForce GTX 1660 GAMING X 6G',         imagen="images/Video/001/001.png",      precioProducto="289990", precioProductoCred="289990",                 descFich="lala"  ,descFichCom="lala",                 categoria=Categoria.objects.get(idCategoria=1))
+#producto 2
+    Producto.objects.create(codProducto="2",
+                            nomProducto='ASUS TUF Gaming F15',        
+                            imagen="images/Notebook/002/002.png",      
+                            precioProducto="869990"  , 
+                            precioProductoCred="899990",                         
+                            descFich="Notebook Asus Tuf Gaming F15 / Fortress Gray / Amd Rayzen 7 / 8 Gb Ram / Nvidia Geforce gtx 1650 ti / 512 Gb / Pantalla '15.6 " ,
+                            descFichCom="lala",                  
+                            categoria=Categoria.objects.get(idCategoria=2))
+#producto 3
+    Producto.objects.create(codProducto="3",
+                            nomProducto='ASUS Chromebook C423NA-WB04',
+                            imagen="images/Notebook/003/003.png",
+                            precioProducto="209990", precioProductoCred="209990",
+                            descFich="Intel Celeron N3350 / Memoria RAM: 4 GB / Disco Duro: 64 GB / Sistema operativo: Chrome OS / Pantalla: '14 (1366x768)"  ,
+                            descFichCom="lala",
+                            categoria=Categoria.objects.get(idCategoria=2))
+#producto 4
+    Producto.objects.create(codProducto="4",
+                            nomProducto='Intel Core i7-11700F',
+                            imagen="images/Procesador/001/001.png",
+                            precioProducto="319900",
+                            precioProductoCred="319900",
+                            descFich="Aumente su productividad, juegos y experiencias de creación de contenido instalando el procesador Intel Core i7-11700F de ocho núcleos LGA 1200 a 2.5 GHz en su sistema informático."  ,
+                            descFichCom="lala",
+                            categoria=Categoria.objects.get(idCategoria=1))
+    #producto 5
+    Producto.objects.create(codProducto="5",
+                            nomProducto='GSkill Trident Z',
+                            imagen="images/ram/001/001.png",
+                            imagen2="images/ram/001/001c.png",
+                            precioProducto="59990"  ,
+                            precioProductoCred="59990",
+                            descFich="Revela tus colores interiores \n ¡Crea una PC única con el kit de memoria Trident Z RGB! Lo mejor de tener capacidades RGB es la facultad de elegir los colores que desee. Ya sea blanco en un día o verde en el siguiente, puede estar seguro de que estos módulos se verán actuales y modernos en cualquier compilación."  ,
+                            descFichCom="lala",
+                            categoria=Categoria.objects.get(idCategoria=1))
+    #producto 6
+    Producto.objects.create(codProducto="6",
+                            nomProducto='MSI GeForce GTX 1660 GAMING X 6G',
+                            imagen="images/Video/001/001.png",
+                            precioProducto="289990", precioProductoCred="289990",
+                            descFich="Prepárate para jugar los últimos juegos con GeForce GTX 1660 Ti, con un desempeño que rivaliza a las GeForce GTX 1070."  ,
+                            descFichCom="lala",
+                            categoria=Categoria.objects.get(idCategoria=1))
+    
     return redirect(producto, action='ins', id = '-1')
