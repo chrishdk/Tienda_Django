b930f556baf1c875443a1de40fdc61cf32184d07
chrishdk <daniel.stari@hotmail.com>
2021-07-02 18:47:17 -0400

    a

--- Tienda/core/forms.py
+++ Tienda/core/forms.py
@@ -1,11 +1,19 @@
 from django import forms
 from django.forms import ModelForm, fields
-from .models import Producto, Usuario
+from .models import  Usuario, Producto
  
 class ProductoForm(ModelForm):
     class Meta:
         model = Producto
-        fields = ['codProducto', 'nomProducto', 'imagen', 'precioProducto','precioProductoCred', 'categoria']
+        fields = ['codProducto',
+                    'nomProducto',
+                    'imagen',
+                    'precioProducto',
+                    'precioProductoCred',
+                    'descFich',
+                    'descFichCom',                    
+                    'categoria'
+                ]
 
 class ValidarUsuarioForm(ModelForm):
     class Meta:
