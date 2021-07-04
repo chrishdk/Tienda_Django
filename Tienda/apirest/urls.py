eeab2aff540b5e410a4f7b092bff1a510b9995ff
mati012 <mati.quiero.arevalo@gmail.com>
2021-07-04 02:48:32 +0200

    a

--- Tienda/apirest/urls.py
+++ Tienda/apirest/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from django.contrib import admin
-from .views import producto_create, producto_read, producto_read_all, producto_update, producto_delete
+from .views import producto_create, producto_read, producto_read_all, producto_update, producto_delete,login
 
 urlpatterns = [
     path('producto_create/', producto_create.as_view(), name="producto_create"),
@@ -8,4 +8,5 @@ urlpatterns = [
     path('producto_read/<id>/', producto_read, name="producto_read"),
     path('producto_read_all/', producto_read_all, name='producto_read_all'),
     path('producto_delete/<id>/', producto_delete, name="producto_delete"),
+    path('login/', login, name='login')
 ]
--- Tienda/apirest/views.py
+++ Tienda/apirest/views.py
@@ -1,15 +1,19 @@
+from django.http.response import JsonResponse
 from django.shortcuts import render, redirect, get_object_or_404
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view,authentication_classes, permission_classes
 from rest_framework.response import Response
-from rest_framework.parsers import JSONParser
+from rest_framework.parsers import DataAndFiles, JSONParser
 from django.views.decorators.csrf import csrf_exempt
 from core.models import Producto
 from .serializers import ProductoSerializer
 from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework.views import APIView
-
-
+from django.contrib.auth.models import User
+from django.contrib.auth.hashers import check_password
+from rest_framework.authtoken.models import Token
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 # Create your views here.
 
 class producto_create(APIView):
@@ -27,8 +31,9 @@ class producto_update(APIView):
             serializer.update()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
+##@csrf_exempt
 @api_view(['GET'])
+##@permission_classes((IsAuthenticated),)
 def producto_read(request, id):
     if request.method == 'GET':
         objeto = get_object_or_404(Producto, codProducto=id)
@@ -50,3 +55,20 @@ def producto_delete(request, id):
             return Response(status=status.HTTP_204_NO_CONTENT)
         except Producto.DoesNotExist:
             return Response(status=status.HTTP_404_NOT_FOUND)
+
+@api_view(['POST'])
+def login (request):
+    data = JSONParser().parse(request)
+    username= data['username']
+    password= data['password']
+    try:
+        user = User.objects.get(username=username)
+    except User.DoesNotExist:
+        return Response ("Usuario no valido")
+    password_valida = check_password(password, user.password)
+    if not password_valida:
+       return Response("contraseña incorrecta")
+    token, created = Token.objects.get_or_create(user=user)
+    print(f"este es el token creado: '{token.key}'" )
+    return Response(token.key)                
+
